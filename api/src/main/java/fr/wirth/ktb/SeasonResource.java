96cd21e46d82677639c4bd0833940e2fa12bfd68
cyril wirth <cyrilwirth@unknown.home>
2021-06-13 22:48:35 +0200

    chore: can generate token command line + login secured

--- api/src/main/java/fr/wirth/ktb/SeasonResource.java
+++ api/src/main/java/fr/wirth/ktb/SeasonResource.java
@@ -1,16 +1,30 @@
 package fr.wirth.ktb;
 
+import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.SecurityContext;
 
 @Path("/seasons")
 public class SeasonResource {
 
     @GET
+    @PermitAll
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello RESTEasy";
+    }
+
+
+    @GET
+    @Path("/secured")
+    @RolesAllowed("admin")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello2(@Context SecurityContext securityContext) {
+        return "Hello " + securityContext.getUserPrincipal().getName();
     }
 }
---
+++ api/src/main/java/fr/wirth/ktb/domain/User.java
@@ -0,0 +1,29 @@
+package fr.wirth.ktb.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+
+@Entity
+@Table(name = "users")
+public class User extends PanacheEntity {
+    public String username;
+    public String password;
+    public String role;
+
+    /**
+     * Adds a new user in the database
+     * @param username the user name
+     * @param password the unencrypted password (it will be encrypted with bcrypt)
+     * @param role the comma-separated roles
+     */
+    public static void add(String username, String password, String role) {
+        User user = new User();
+        user.username = username;
+        user.password = password; //TODO move on bcrypt
+        user.role = role;
+        user.persist();
+    }
+}
